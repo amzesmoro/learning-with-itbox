33fce6e37834d1e24b81d310d3a4c52387c7121e
amzesmoro <amzesmoro05@gmail.com>
2022-02-09 15:33:05 +0700

    add home screen, models, and dummy data

---
+++ menengah/notes_app/lib/models/note.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class Note {
+  final String id;
+  final String title;
+  final String note;
+  final DateTime updatedAt;
+  final DateTime createdAt;
+  bool isPinned;
+
+  Note({
+    @required this.id,
+    @required this.title,
+    @required this.note,
+    @required this.updatedAt,
+    @required this.createdAt,
+    this.isPinned = false,
+  });
+}
